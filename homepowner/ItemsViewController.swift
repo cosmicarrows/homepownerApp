0428f0d3b4535b867d0cef80764ad96394d2c01a
Laurence Wingo <cosmicarrowsllc@cosmics-mbp.gsu.edu>
2018-04-29 19:33:05 -0400

    Created ItemsViewController.swift file.  Deleted the initial viewController in the storyboard that comes with a single view application.  Added a TableViewController object to the storyboard and changed its class to the ItemsViewController.swift file.  Also made this TableViewController the initial viewController to load on launch from within the storyboard attributes and not programmatically.

---
+++ homepowner/ItemsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ItemsViewController.swift
+//  homepowner
+//
+//  Created by Cosmic Arrows, LLC on 4/29/18.
+//  Copyright © 2018 Cosmic Arrows, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
