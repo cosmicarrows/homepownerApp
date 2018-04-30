f12c86f962b427f1cd30ed0fa65ba4f89da7accc
Laurence Wingo <cosmicarrowsllc@cosmics-macbook-pro.local>
2018-04-30 17:46:18 -0400

    Implemented The Cell For Row Method on ItemsViewController
    In order for the items to show in the ItemsViewController's tableView I needed to add the method cellForRowAtIndex() which returns a UITableViewCell determining the design of each tableViewCell

--- homepowner/ItemsViewController.swift
+++ homepowner/ItemsViewController.swift
@@ -12,24 +12,36 @@ import UIKit
 class ItemsViewController: UITableViewController {
     
     //to give the controller access to the ItemStore
+    //the ! means its an implicitly unwrapped optional meaning use this guy once and use the value anywhere you want
     var itemStore: ItemStore!
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
     
+    // MARK: - Implement Required UITableViewDataSource Methods from the UITableViewDataSource Protocol.
+    
+    //UITableViewController subclasses already come conformed to UITableViewDataSoure and UITableViewDelegate Protocols just in case you were wondering why the protocol conformity isn't shown at the top of the ItemsViewController class.
+   
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        switch section {
-        case 0:
-            return itemStore.allItems.count
-        case 1:
-            return 2
-        default:
-            print("still working on this logical decision if a switch statement does what we need in order to have two sections")
-            return 8
-            
-        }
+        return itemStore.allItems.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell, with default appearance
+        let cell = UITableViewCell.init(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        // Set the text on the cell with the description of the item instance that is at the nth index of the allItems array, where n = row this cell will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        
+        return cell
     }
     
 }
+
+
+
+
+
