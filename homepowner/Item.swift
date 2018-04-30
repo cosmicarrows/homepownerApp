d5249ab8daacd730a247fc39416f4a9c222d063a
Laurence Wingo <cosmicarrowsllc@cosmics-mbp.gsu.edu>
2018-04-29 20:07:37 -0400

    We just added a convenience initializer for when we need random instances for when the app starts in demo mode.

--- homepowner/Item.swift
+++ homepowner/Item.swift
@@ -32,6 +32,41 @@ class Item: NSObject {
         super.init()
     }
     
+    //now we will create a convenience initializer as an optional helper for situations where we want to instantiate randomly generated Item instances
+    convenience init(random: Bool = false) {
+        //this convenience initializer has an argument called random which is set to false as the default value
+        
+        //now we will enter the logic in the event that the random argument is changed to true during instantiation
+        if random {
+            let adjectives = ["Odd", "Wild", "Tough"]
+            let nouns = ["Red Tribe Bike", "Vegan Pizza", "Macbook Pro"]
+            //since arrays are typcially integer based, we needed to cast adjectives.count to a UInt32 type in order to use its value within the arc4random_uniform method.
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            //now we're casting this value back into an Int to be used as an index for the adjectives array
+            let randomAdjective = adjectives[Int(idx)]
+            
+            //now we'll re-use the idx variable to randomly select a noun from the nouns array
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            //now we'll concatenate the two randomly chosen terms into this randomName constant
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            
+            //now we'll create a random value for the valueInDollars property of the Item instance
+            let randomValue = Int(arc4random_uniform(1000))
+            //now we'll create a random serial number using UUID's
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            //now to make this convenience initializer fully functional we'll need to call the designated initializer by saying self.init and fill in the parameters based on the values we just generated above.
+            
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+            
+        }
+        else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
+    
     
 }
 
